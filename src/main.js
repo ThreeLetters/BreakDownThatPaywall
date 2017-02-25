ef3a65eaf747a0a9901e3264a5e14a9002d7c100
Andrew S <andrews54757@gmail.com>
2017-02-25 01:23:43 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 
+var url = window.location.href ;
+var header = false;
+if (url.indexOf("wsj.com") != -1) {
+ header = "https://www.twitter.com/";
+} else {
+   header = "https://www.google.com/";
+}
